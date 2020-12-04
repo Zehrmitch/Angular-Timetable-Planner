f830d04e84d3122bcc3ecb6e9af84f01073e4591
Mitchell Zehr <zehrmitch@gmail.com>
2020-12-04 13:44:17 -0500

    Add list constructor to schedules
    Lay out varribles for schedules and some basic database access code

--- src/app/schedules/schedules.component.ts
+++ src/app/schedules/schedules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 
 @Component({
   selector: 'app-schedules',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulesComponent implements OnInit {
 
+  newScheduleName: string;
+  isPublic: boolean;
+  description: string;
+
+  deleteScheduleName: string;
+  
+  timetableName: string;
+  timetable: any[];
+  searchSubject: string = "";
+  searchCode: string = "";
+  searchComponent: string = "";
+
+  savedCourses: any;
+  temp :any;
+  courseCodes: any[] = [];
+  subjects: any[] = [];
+  components: any[] = [];
+  startTimes: any[] = [];
+  endTimes: any[] = [];
+  days: any[] =[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createSchedule() {
+    var data = {
+      listName: "",
+      creator: "",
+      lastEdit: "",
+      numCourses: false,
+      courses: true,
+      visible: true,
+    }
+
+    //return new Promise<any>((resolve, reject) =>{
+       // this.firestore.collection("users").add(data)
+   //         .then(res => {
+   //           alert("user added");
+    //        }, err => reject(err));
+   // });
   }
 
 }
